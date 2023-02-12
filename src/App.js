2c8a2bfc5bf218a53aeed9981f36ef807aeb6b1c
Duc Cuong <cuongtu532@gmail.com>
2023-02-12 14:13:20 +0700

    text

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="title">
+        <div class="heart-1 heart"></div>
+        <div class="heart-2 heart"></div>
+        <div class="heart-3 heart"></div>
+        <div class="heart-4 heart"></div>
+        <div class="heart-5 heart"></div>
+        <div class="heart-6 heart"></div>
+        <div class="heart-7 heart"></div>
+      </div>
+      <div class="main">
+
+        <div class="container">
+          <div class="envelope"></div>
+          <div class="card">
+            <h1 class="message"><span>Happy Valentine's Day!</span></h1>
+            <div class="pikachu">
+              <div class="limbs"></div>
+              <div class="heart"></div>
+              <div class="arms"></div>
+              <div class="smile"></div>
+            </div>
+          </div>
+          <div class="cover"></div>
+          <div class="lid"></div>
+          <div class="shadow"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
